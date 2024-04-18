0cf71fddf85d6b3745e079dda3faa7de65d34ab7
Abu Bakar Istiak <151659711+abubakaristiak@users.noreply.github.com>
2024-04-18 22:45:40 +0600

    HackerRank

---
+++ _42_Sum_Sum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    int even_sum=0, odd_sum=0;
+    scanf("%d", &n);
+    int ar[n];
+    for(int i=0; i<n; i++){
+        scanf("%d", &ar[i]);
+    }
+    for(int i=0; i<n; i++){
+        if(ar[i]>0){
+            even_sum = even_sum +ar[i];
+        }
+        else if(ar[i]<0){
+            odd_sum = odd_sum+ar[i];
+        }
+    }
+    printf("%d %d",even_sum, odd_sum);
+}
---
+++ _43_Update_and_Print.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n, i;
+    scanf("%d", &n);
+    int ar[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &ar[i]);
+    }
+    int x, y;
+    scanf("%d %d", &x, &y);
+    ar[x] = y;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        printf("%d ", ar[i]);
+    }
+}
